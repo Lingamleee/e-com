654e2a07e754e63954a30788dc59deb87a2dfbb0
Lingamleee <lingamlinga10019@gmail.com>
2022-09-13 12:03:30 +0530

    first commit

---
+++ src/clint.js
@@ -0,0 +1,43 @@
+import fetch from "unfetch";
+
+export const getAllProducts = [
+	{
+		id: 1,
+		title: "Smart Watch",
+		qty: 2,
+		img:
+			"https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/content-link-watch-workout_2x.png",
+		price: 10000,
+	},
+	{
+		id: 2,
+		title: "Smart Watch",
+		qty: 2,
+		img:
+			"https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/content-link-watch-workout_2x.png",
+		price: 10000,
+	},
+	{
+		id: 3,
+		title: "Smart Watch",
+		qty: 2,
+		img:
+			"https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/content-link-watch-workout_2x.png",
+		price: 10000,
+	},
+	{
+		id: 4,
+		title: "Smart Watch",
+		qty: 2,
+		img:
+			"https://support.apple.com/content/dam/edam/applecare/images/en_US/psp/content-link-watch-workout_2x.png",
+		price: 10000,
+	},
+	{
+		id: 5,
+		title: "Shoe",
+		qty: 1,
+		img: "https://m.media-amazon.com/images/I/81-fNmQqlLL._SX679_.jpg",
+		price: 10000,
+	},
+];
